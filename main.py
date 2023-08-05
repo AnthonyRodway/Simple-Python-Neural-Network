b347597be843529b97717be02e4ce3291b596eab
anthonyrodway <anthonyrodway@uvic.ca>
2023-08-05 14:40:06 -0700

    Add program timer and refactor neuron calculation

--- main.py
+++ main.py
@@ -1,22 +1,20 @@
 import sys
 import numpy as np
 import matplotlib
+import time
 
 
 def main():
     inputs = [1, 2, 3, 2.5]
-    weights1 = [0.2, 0.8, -0.5, 1.0]
-    weights2 = [0.5, -0.91, 0.26, -0.5]
-    weights3 = [-0.26, -0.27, 0.17, 0.87]
-    bias1 = 2
-    bias2 = 3
-    bias3 = 0.5
-
-    output = [
-        np.dot(inputs, weights1) + bias1,
-        np.dot(inputs, weights2) + bias2,
-        np.dot(inputs, weights3) + bias3,
+    weights = [
+        [0.2, 0.8, -0.5, 1.0],
+        [0.5, -0.91, 0.26, -0.5],
+        [-0.26, -0.27, 0.17, 0.87],
     ]
+    biases = [2, 3, 0.5]
+
+    output = np.dot(weights, inputs) + biases
+
     print(output)
 
 
@@ -26,5 +24,15 @@ if __name__ == "__main__":
     # print("Numpy: ", np.__version__)
     # print("Matplotlib: ", matplotlib.__version__)
 
+    # Get the start time of the program
+    start = time.time()
+
     # Call the main file function
     main()
+
+    # Get the end time of the program
+    end = time.time()
+
+    # Calculate the elapsed time
+    elapsed = end - start
+    print(f"Elapsed time: {elapsed:.3f} seconds")
