0c139d10a2de6b700888f756bdf4bdfb059a5bf9
anthonyrodway <anthonyrodway@uvic.ca>
2023-08-05 14:05:44 -0700

    Expanding to multiple neurons

--- main.py
+++ main.py
@@ -4,11 +4,19 @@ import matplotlib
 
 
 def main():
-    inputs = [1.2, 5.1, 2.1]
-    weights = [3.1, 2.3, 4.5]
-    bias = 3
+    inputs = [1, 2, 3, 2.5]
+    weights1 = [0.2, 0.8, -0.5, 1.0]
+    weights2 = [0.5, -0.91, 0.26, -0.5]
+    weights3 = [-0.26, -0.27, 0.17, 0.87]
+    bias1 = 2
+    bias2 = 3
+    bias3 = 0.5
 
-    output = np.dot(inputs, weights) + bias
+    output = [
+        np.dot(inputs, weights1) + bias1,
+        np.dot(inputs, weights2) + bias2,
+        np.dot(inputs, weights3) + bias3,
+    ]
     print(output)
 
 
