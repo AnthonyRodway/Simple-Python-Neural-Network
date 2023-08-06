e6a98a150c567d31050c095fb9a943fa03048cb4
anthonyrodway <anthonyrodway@uvic.ca>
2023-08-05 19:42:34 -0700

    Adding second layer of neurons and switching to batches for input

--- main.py
+++ main.py
@@ -5,17 +5,33 @@ import time
 
 
 def main():
-    inputs = [1, 2, 3, 2.5]
+    inputs = [
+        [1.0, 2.0, 3.0, 2.5],
+        [2.0, 5.0, -1.0, 2.0],
+        [-1.5, 2.7, 3.3, -0.8],
+    ]
+
     weights = [
         [0.2, 0.8, -0.5, 1.0],
         [0.5, -0.91, 0.26, -0.5],
         [-0.26, -0.27, 0.17, 0.87],
     ]
-    biases = [2, 3, 0.5]
+    biases = [2.0, 3.0, 0.5]
 
-    output = np.dot(weights, inputs) + biases
+    weights2 = [
+        [0.1, 0.2, -0.3],
+        [0.6, -0.5, 0.6],
+        [-0.1, -0.9, 0.7],
+    ]
+    biases2 = [-1.0, 1.0, -0.5]
 
-    print(output)
+    layer1_outputs = np.dot(inputs, np.transpose(weights)) + biases
+
+    print("Layer1: ", layer1_outputs)
+
+    layer2_outputs = np.dot(layer1_outputs, np.transpose(weights2)) + biases2
+
+    print("Layer2: ", layer2_outputs)
 
 
 if __name__ == "__main__":
